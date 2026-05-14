3666c95506e63668335a82f98f6c2635b29c1474
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2026-05-14 21:30:10 +0100

    build

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/Users/ben/dev/dowenb.github.io/tina/__generated__/.cache/1778790365923', url: 'https://content.tinajs.io/2.4/content/7d99bd01-0ec7-4d7c-8568-e6e2c0f14d37/github/main', token: '368f3b94b77f44e9878a9a8944b63f37cec5f227', queries,  });
+export const client = createClient({ cacheDir: '/Users/ben/dev/dowenb.github.io/tina/__generated__/.cache/1778790573151', url: 'https://content.tinajs.io/2.4/content/7d99bd01-0ec7-4d7c-8568-e6e2c0f14d37/github/main', token: '368f3b94b77f44e9878a9a8944b63f37cec5f227', queries,  });
 export default client;
   
